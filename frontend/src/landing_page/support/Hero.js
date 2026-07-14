a0a56f500d75d34d5263c9753a45745b05f15236
Prathmesh Pawar <prathmeshpawar3182@gmail.com>
2026-07-14 09:22:28 +0530

    Fix frontend deployment

--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -1,32 +1,44 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Hero() {
   return (
     <section className="container-fluid" id="supportHero">
-      <div className="p-5 " id="supportWrapper">
+      <div className="p-5" id="supportWrapper">
         <h4>Support Portal</h4>
-        <a href="">Track Tickets</a>
+        <Link to="/">Track Tickets</Link>
       </div>
+
       <div className="row p-5 m-3">
         <div className="col-6 p-3">
           <h1 className="fs-3">
             Search for an answer or browse help topics to create a ticket
           </h1>
+
           <input placeholder="Eg. how do I activate F&O" />
+
           <br />
-          <a href="">Track account opening</a>
-          <a href="">Track segment activation</a>
-          <a href="">Intraday margins</a>
-          <a href="">Kite user manual</a>
+
+          <Link to="/">Track account opening</Link>{" "}
+          <Link to="/">Track segment activation</Link>{" "}
+          <Link to="/">Intraday margins</Link>{" "}
+          <Link to="/">Kite user manual</Link>
         </div>
+
         <div className="col-6 p-3">
           <h1 className="fs-3">Featured</h1>
+
           <ol>
             <li>
-              <a href="">Current Takeovers and Delisting - January 2024</a>
+              <Link to="/">
+                Current Takeovers and Delisting - January 2024
+              </Link>
             </li>
+
             <li>
-              <a href="">Latest Intraday leverages - MIS & CO</a>
+              <Link to="/">
+                Latest Intraday leverages - MIS & CO
+              </Link>
             </li>
           </ol>
         </div>
